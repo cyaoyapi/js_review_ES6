def4914fe0a81d4742ca221d49a83dee843ff44a
Cyrille YAO <cyaoyapi@gmail.com>
2019-01-29 19:34:33 +0000

    Preven default behavior.

--- 009_Events_managing/script.js
+++ 009_Events_managing/script.js
@@ -7,9 +7,11 @@ let clic2 = document.getElementById('clic2');
 
 // Adding Event Listerners
 clic1.addEventListener('click', function(event){
+	event.preventDefault();
 	alert("Clic me !");
 })
 
 clic2.addEventListener('click', event => {
+	event.preventDefault();
 	alert("I repeat again : Clic me !");
 })
