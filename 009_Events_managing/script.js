011a4465573e1ad84c7f3ee4aec3927625de6fab
Cyrille YAO <cyaoyapi@gmail.com>
2019-01-29 19:36:19 +0000

    Stop propagation.

--- 009_Events_managing/script.js
+++ 009_Events_managing/script.js
@@ -8,10 +8,12 @@ let clic2 = document.getElementById('clic2');
 // Adding Event Listerners
 clic1.addEventListener('click', function(event){
 	event.preventDefault();
+	event.stopPropagation();
 	alert("Clic me !");
 })
 
 clic2.addEventListener('click', event => {
 	event.preventDefault();
+	event.stopPropagation();
 	alert("I repeat again : Clic me !");
 })
