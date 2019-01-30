c065036d86694a614321bc0fa5efed495b73ca71
Cyrille YAO <cyaoyapi@gmail.com>
2019-01-30 12:15:13 +0000

    Asynchronous codes managed by promises.

--- 015_Handling_Asynchronous_Codes/script.js
+++ 015_Handling_Asynchronous_Codes/script.js
@@ -1,8 +1,35 @@
+const sideEffectsManager = function(event){
+	event.preventDefault();
+	event.stopPropagation();
+}
+
+// Asynchronus : Using callback functions
 let btn_callback = document.getElementById("btn_callback");
-btn_callback.addEventListener('click', function(){
-     alert("Events' Listeners are asynchronous functions using callbacks");
-     window.setTimeout(function(){
-          alert("setTimeout, setInterval,...are also asynchronous functions using callbacks");
-     }, 1000);
+btn_callback.addEventListener('click', function(e){
+	sideEffectsManager(e);
+    alert("Events' Listeners are asynchronous functions using callbacks");
+     	window.setTimeout(function(){
+          	alert("setTimeout, setInterval,...are also asynchronous functions using callbacks");
+    }, 1000);
+
+}, false);
+
+// Asynchronus : Using callback promises
+let btn_promise = document.getElementById("btn_promise");
+btn_promise.addEventListener('click', function(e){
+	sideEffectsManager(e);
+	let myPromise = new Promise((resolve, reject) => {
+		alert("Promise test : Initial...");
+		resolve();
+	});
+
+    myPromise
+    .then(() => alert("Promise OK!"))
+    .then(() => {
+    	throw new Error("Promise Error!");
+    	alert("Do not excute this!");
+    })
+    .catch(() => alert("Promise Error!"))
+    .then(() => alert("All is good!"))
 
 }, false);
