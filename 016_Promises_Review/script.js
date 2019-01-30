53fc33a96f7bc90deb7556a58957f26a33b9d432
Cyrille YAO <cyaoyapi@gmail.com>
2019-01-30 18:19:17 +0000

    Parallelism with promise.

--- 016_Promises_Review/script.js
+++ 016_Promises_Review/script.js
@@ -1,24 +1,15 @@
 /* Promises review.
 */
-let divinePromise = new Promise(function(resolve, reject){
-		let flag = Math.floor(Math.random() * 10)
-		if(flag > 5){
-			resolve();
-		}
-		else{
-			reject();
-		}
-	
+
+// Old way to make asynchronous with callback function
+
+
+async function randomValue(){
+	return Math.floor(Math.random() * 10);
+}
+
+Promise
+.all([randomValue(), randomValue()])
+.then(function(results){
+	console.log(`${results[0]} + ${results[1]} = ${results[0] + results[1]}`);
 });
-
-function neverFails(){
-	console.log("God's promise never fails");
-}
-
-function delayed_promise() {
-	console.log("If God's promise delays, believe and wait, it never fails.");
-}
-
-
-divinePromise.then(neverFails, delayed_promise);
-
