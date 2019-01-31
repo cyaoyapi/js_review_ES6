73ebd01e87c877e60a7fc44db65931555e593b32
Cyrille YAO <cyaoyapi@gmail.com>
2019-01-31 13:59:47 +0000

    Add babel in webpack config

--- 017_OptimizedCode/webpack.config.js
+++ 017_OptimizedCode/webpack.config.js
@@ -3,10 +3,21 @@ const path = require('path');
 module.exports = {
 	mode: "production",
 	entry: {
+		polyfill: "babel-polyfill",
 		app: "./src/index.js"
 	},
 	output: {
 		filename: "[name].bundle.js",
 		path:path.resolve(__dirname, "dist")
+	},
+	module: {
+		test: \\.js$\,
+		exclude: '/node_modules/',
+		use: {
+			loader: "babel-loader",
+			options: {
+				presets: ["@babel/preset-env"]
+			}
+		}
 	}
 };
