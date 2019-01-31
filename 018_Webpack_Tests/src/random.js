e723811e3a7123bda99268582ee281d5b2e6ad03
Cyrille YAO <cyaoyapi@gmail.com>
2019-01-31 19:05:29 +0000

    Fix logical bug.

--- 018_Webpack_Tests/src/random.js
+++ 018_Webpack_Tests/src/random.js
@@ -1,6 +1,6 @@
 async function randomValue(){
 	// Return an integer between 1 and 10
-	return Math.floor(Math.random() * 10) + 11;
+	return Math.floor(Math.random() * 10) + 1;
 }
 
 export default randomValue;
