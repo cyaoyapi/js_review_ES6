ab894b1c363d691b316e3473de0e28a8374b0425
Cyrille YAO <cyaoyapi@gmail.com>
2019-01-31 19:07:04 +0000

    Fix syntax bug

--- 018_Webpack_Tests/src/index.js
+++ 018_Webpack_Tests/src/index.js
@@ -4,7 +4,7 @@ async function sayHello(){
 	let counter = await randomValue();
 	console.log(`Selected number : ${counter}`);
 	for(let i = 1; i <= counter; i++){
-		console.log(`{i} time(s) => Hello`);
+		console.log(`${i} time(s) => Hello`);
 	}
 }
 
