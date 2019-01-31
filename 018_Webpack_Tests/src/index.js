0c4dbb711aa161ce540c66c2cb83e383962e6db7
Cyrille YAO <cyaoyapi@gmail.com>
2019-01-31 21:43:24 +0000

    It works : well done!

--- 018_Webpack_Tests/src/index.js
+++ 018_Webpack_Tests/src/index.js
@@ -1,11 +1,12 @@
 import randomValue from './random.js';
+import style from './style.css';
 
 async function sayHello(){
 	let elt = document.createElement("div");
 	let counter = await randomValue();
-	elt.innerHTML += `Selected number : ${counter}<br>`;
+	elt.innerHTML += `<p>Selected number : ${counter}</p>`;
 	for(let i = 1; i <= counter; i++){
-		elt.innerHTML += `${i} time(s) => Hello<br>`;
+		elt.innerHTML += `<p>${i} time(s) => <span>Hello</span></p>`;
 	}
 	document.getElementsByTagName("body")[0].appendChild(elt);
 }
