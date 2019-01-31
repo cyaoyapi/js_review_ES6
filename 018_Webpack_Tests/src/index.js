d2b152133f02e616b3f4e2c608a9ee2830516c37
Cyrille YAO <cyaoyapi@gmail.com>
2019-01-31 19:47:32 +0000

    Input in the DOM.

--- 018_Webpack_Tests/src/index.js
+++ 018_Webpack_Tests/src/index.js
@@ -1,11 +1,13 @@
 import randomValue from './random.js';
 
 async function sayHello(){
+	let elt = document.createElement("div");
 	let counter = await randomValue();
 	console.log(`Selected number : ${counter}`);
 	for(let i = 1; i <= counter; i++){
-		console.log(`${i} time(s) => Hello`);
+		elt.innerHTML += `${i} time(s) => Hello`;
 	}
+	document.getElementsByTagName("body")[0].appendChild(elt);
 }
 
 sayHello();
