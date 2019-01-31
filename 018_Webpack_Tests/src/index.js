6200c03d81f637667bab8c55cdf75f1959d27dc4
Cyrille YAO <cyaoyapi@gmail.com>
2019-01-31 20:11:41 +0000

    Install css-loader style-loader

--- 018_Webpack_Tests/src/index.js
+++ 018_Webpack_Tests/src/index.js
@@ -3,9 +3,9 @@ import randomValue from './random.js';
 async function sayHello(){
 	let elt = document.createElement("div");
 	let counter = await randomValue();
-	console.log(`Selected number : ${counter}`);
+	elt.innerHTML += `Selected number : ${counter}<br>`;
 	for(let i = 1; i <= counter; i++){
-		elt.innerHTML += `${i} time(s) => Hello`;
+		elt.innerHTML += `${i} time(s) => Hello<br>`;
 	}
 	document.getElementsByTagName("body")[0].appendChild(elt);
 }
